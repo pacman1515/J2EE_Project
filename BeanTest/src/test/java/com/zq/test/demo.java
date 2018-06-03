3e74b8ca87bc35e7338ce62d4704170e602c0cae
tamperMonkeyZQ <includeiostreamzq@gmail.com>
2018-06-03 17:59:41 +0800

    demo测试

--- BeanTest/src/test/java/com/zq/test/demo.java
+++ BeanTest/src/test/java/com/zq/test/demo.java
@@ -1,6 +1,6 @@
 package com.zq.test;
 
-import com.zq.controller.BeanTest;
+import com.zq.bean.BeanTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
